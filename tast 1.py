02673152c6848797162037764ac55e1e152b308d
Jagtar Singh <singh.js421@gmail.com>
2025-08-15 10:08:51 -0700

    Assignment 1 Task 1 and Task 2

---
+++ tast 1.py
@@ -0,0 +1,11 @@
+print('Hello Sir')
+num1 = float(input('Enter first number: '))
+num2 = float(input('Enter second number: '))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print(f'Addition : {addition}')
+print(f'Subtraction : {subtraction} ')
+print(f'Multiplication : {multiplication} ')
+print(f'Division : {division} ')
